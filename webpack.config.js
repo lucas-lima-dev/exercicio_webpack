366f1476c491169bb7509caa151a542e774965c9
lucas-lima-dev <lucas.sdslima@gmail.com>
2022-12-22 19:31:40 -0300

    add packages webpack and babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,20 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
   mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
   devtool: process.env.NODE_ENV === 'development' ?  'eval-cheap-module-source-map' : 'source-map'
 }
